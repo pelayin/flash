835328607d14a38cebd8812f2e0416d168323fd7
pelayin <pelaez.malaga@gmail.com>
2017-06-28 01:30:10 +0200

    lenguaje ek agregado

---
+++ resources/lang/ek/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+     */
+
+    'failed'   => 'En Eisguarri te darian dos hostias.',
+    'throttle' => 'No me jodas y deja de tocar el botoncito',
+
+];
