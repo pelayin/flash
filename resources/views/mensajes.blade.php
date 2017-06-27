4b468454cb1cee4a9b857daa80f415aecff48391
pelayin <pelaez.malaga@gmail.com>
2017-06-28 00:45:14 +0200

    version inicial del proyecto

---
+++ resources/views/mensajes.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Bootstrap - notification popup box using bootstrap-growl JS</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container text-center">
+            <br/><h2>Bootstrap - notification popup box using bootstrap-growl JS Plugin</h2><br/>
+            {{-- <button class="success btn btn-success">Success</button>
+            <button class="error btn btn-danger">Error</button>
+            <button class="info btn btn-info">Info</button>
+            <button class="warning btn btn-warning">Warning</button> --}}
+        </div>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+        {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js"></script> --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.13/push.min.js"></script>
+        <script>Push.create('Hola mundo, me la pela con un pelador')</script>
+    </body>
+</html>
